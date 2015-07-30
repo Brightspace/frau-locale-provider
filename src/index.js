df086fbfe829788a3fcbd6581d9a7155c6b4c49e
Dave Lockhart <dave.lockhart@d2l.com>
2015-07-30 12:16:22 -0400

    cleaning up by moving logic out of getLangTag

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 'use strict';
 
-var isFramed = require('./isFramed'),
+var getHtmlLangFramed = require('./framed/getHtmlLang'),
+	getHtmlLangLocal = require('./local/getHtmlLang'),
+	isFramed = require('./isFramed'),
 	isRtlLocal = require('./local/isRtl'),
-	isRtlFramed = require('./framed/isRtl');
+	isRtlFramed = require('./framed/isRtl'),
+	resolveLang = require('./resolveLang');
 
-module.exports.getLangTag = require('./getLangTag').get;
-module.exports.isRtl = function() {
-	var isRtl = isFramed() ? isRtlFramed : isRtlLocal;
-	return isRtl();
+var exports = {
+	getLangTag: function() {
+		var getHtmlLang = exports._isFramed() ?
+			exports._getHtmlLangFramed : exports._getHtmlLangLocal;
+		return getHtmlLang().then(function(value) {
+			var langTag = exports._resolveLang(
+				value.lang,
+				value.fallback
+			);
+			return langTag;
+		});
+	},
+	isRtl: function() {
+		var isRtl = exports._isFramed() ?
+			exports._isRtlFramed : exports._isRtlLocal;
+		return isRtl();
+	},
+	_getHtmlLangFramed: getHtmlLangFramed,
+	_getHtmlLangLocal: getHtmlLangLocal,
+	_isFramed: isFramed,
+	_isRtlFramed: isRtlFramed,
+	_isRtlLocal: isRtlLocal,
+	_resolveLang: resolveLang
 };
+
+module.exports = exports;
