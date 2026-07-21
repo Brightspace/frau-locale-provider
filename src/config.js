823dc67e22e184b5297913f81aa912cce4fc9a96
Danny Gleckler <daniel.gleckler@d2l.com>
2026-07-21 09:42:58 -0400

    Normalize "regions"

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var supported = require('@brightspace-ui/intl/lib/locale-data/supported.js');
+var normalizeLangTag = require('./normalizeLangTag');
 
 module.exports = {
 	defaultLangTag: 'en',
 	primary: supported.supportedBaseLocales,
-	regions: supported.supportedLocales,
+	regions: supported.supportedLocales.map(l => normalizeLangTag(l)),
 	aliases: {
 		'en-AU': 'en-GB'
 	}
